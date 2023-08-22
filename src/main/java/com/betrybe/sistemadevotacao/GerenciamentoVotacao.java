ff386352eadd715a31c06d0ecd683b3d63ae5fc6
miguelraatz <miguelraatz_@hotmail.com>
2023-08-22 15:18:59 -0300

    feat: starting the creation of the class of GerenciamentoDeVotacao requirement 4

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,5 +1,36 @@
 package com.betrybe.sistemadevotacao;
 
-public class GerenciamentoVotacao {
+import java.util.ArrayList;
 
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface{
+
+  private ArrayList<String> pessoasCandidatas;
+  private ArrayList<PessoaEleitora> pessoasEleitoras;
+  private ArrayList<String> cpfsComputados;
+
+  public GerenciamentoVotacao() {
+    this.pessoasCandidatas = new ArrayList<>();
+    this.pessoasEleitoras = new ArrayList<>();
+    this.cpfsComputados = new ArrayList<String>();
+  }
+
+  @Override
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+    pessoasCandidatas.add(nome);
+  }
+
+  @Override
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+
+  }
+
+  @Override
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+
+  }
+
+  @Override
+  public void mostrarResultado() {
+
+  }
 }
